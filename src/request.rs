9a4381645d9a38adcbb07300e19337a90f5eac12
oskar <git@bozgi.space>
2025-04-19 23:42:20 +0200

    Request file setup

--- src/request.rs
+++ src/request.rs
@@ -1,3 +1,11 @@
-struct Request {
+use tokio::net::TcpStream;
 
+pub struct Request {
+    stream: TcpStream
+}
+
+impl Request {
+    pub fn handle_stream(mut stream: TcpStream) {
+        todo!("Implement")
+    }
 }
